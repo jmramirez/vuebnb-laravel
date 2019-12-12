6c1d75bc844d44c68dc0d6509b3bf293673bce12
Jose Ramirez <josemramirez81@outlook.com>
2019-12-11 22:34:50 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## vuebnb-laravel
+
+This is a project made to practice php laravel framework, as well as Vue.js framework.
+
+The idea for this project was taken from the book "Full-Stack Vue.js 2 and Laravel 5" by: Anthony Gore.   
---
+++ resources/js/app.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import { populateAmenitiesAndPrices} from "./helpers";
+import "core-js/es/object/assign"
+let model = JSON.parse(window.vuebnb_listing_model);
+model = populateAmenitiesAndPrices(model);
+
+let app = new Vue({
+  el: '#app',
+  data: Object.assign(model,{
+    headerImageStyle: {
+        'background-image': `url(${model.images[0]})`
+    },
+    contracted: true,
+    modalOpen: false
+  }),
+  methods: {
+    escapeKeyListener: function(evt) {
+      if (evt.keyCode === 27 && this.modalOpen) {
+        this.modalOpen = false;
+      }
+    }
+  },
+  watch: {
+    modalOpen: function() {
+      var className = 'modal-open';
+      if (this.modalOpen) {
+        document.body.classList.add(className);
+      } else {
+        document.body.classList.remove(className);
+      }
+    }
+  },
+  created: function() {
+    document.addEventListener('keyup', this.escapeKeyListener);
+  },
+  destroyed: function () {
+    document.removeEventListener('keyup', this.escapeKeyListener);
+  }
+});
