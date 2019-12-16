b0701b3e067293eb1bb80067dba84f7f25bad90f
Jose Ramirez <josemramirez81@outlook.com>
2019-12-16 16:56:58 -0500

    Setting up Vue Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import "core-js/es/object/assign"
+
 import ListingPage from "./components/ListingPage";
+import router from "./router";
 
 let app = new Vue({
   el: '#app',
-  render: h =>h(ListingPage)
+  render: h =>h(ListingPage),
+  router
 });
